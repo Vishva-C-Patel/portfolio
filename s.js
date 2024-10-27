72e534ee2833b33203fb66e42f9b9ef2e66ed1ee
Vishva-C-Patel <vishvacpatel1300@gmail.com>
2024-10-27 21:44:09 +0530

    Add files via upload

---
+++ s.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        if (sectionTop < window.innerHeight - 100) {
+            section.classList.add('show');
+        }
+    });
+});
